3659c29da3a502aa2bf0e19eae03cfbe5c11a7f8
prateek <119415864+prateek765@users.noreply.github.com>
2023-05-05 22:18:24 +0530

    Update birthday cake candles.cpp

--- birthday cake candles.cpp
+++ birthday cake candles.cpp
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int n, height, max_height = 0, count = 0;
+    int n, height, maxheight = 0, count = 0;
     scanf("%d", &n);
     for(int i = 0; i < n; i++) {
         scanf("%d", &height);
-        if(height > max_height) {
-            max_height = height;
+        if(height > maxheight) {                 //increasing count of maxheight candles by comparing with current height
+            maxheight = height;          
             count = 1;
         }
-        else if(height == max_height) {
-            count++;
+        else if(height == maxheight) {
+            count++;                            
         }
     }
-    printf("%d\n", count);
+    printf("%d\n", count);   //output count of candles with maximum height
     return 0;
-}
-
